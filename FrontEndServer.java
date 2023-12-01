d338b0b68af4bf40193faf03e578d86a98c319ef
Eman Okyere <eokyere@bowdoin.edu>
2023-11-30 20:05:25 -0500

    added more to FrontEnd.java

--- FrontEndServer.java
+++ FrontEndServer.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.net.URL;     
+import java.lang.Math
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
@@ -14,11 +15,10 @@ import org.apache.xmlrpc.server.XmlRpcServer;
  */
 public class FrontEndServer { 
   // Plan of action --> today we need to have multiple distributed machines (so two machines)
-  // 
+
   // Frontend --> decrypt key to add servers to regions
   // manages one main region, and a list of other FrontEndIPs
 
-  //
   // some way to partition categories --> first we're starting off with 10 categories
   // assign database IP to categories
   // add method 
@@ -27,46 +27,47 @@ public class FrontEndServer {
   // list machines to the genre they are in charge of
   private static boolean dataLoaded = false;
   private static List<String> categories = new ArrayList<String>();
+  // instead have a hashfunction with the number of databases
   private static Map<String, ArrayList<String>> categoryToDb = new HashMap<String, ArrayList<String>>(); 
   private static ArrayList<String> databases = new ArrayList<>();
   private static ArrayList<String> otherFronteEnds = new ArrayList<>();
 
+  // hash function even work 
+  // add 
   // clients call this to send as small package/boolean
   public boolean heartbeat() {
     return true;
   }
+
   private void repartion(){
+    // how would repartitioning work 
+    // hash with old vslue
+  
     return;
+  }
+
+  // hash function, is send string to hash ---> hash sends out 
+  public int hash(String category){
+    // doing hashing
+    // taken from https://stackoverflow.com/questions/16521148/string-to-unique-integer-hashing
+    int result = 0;
+    for (int i = 0; i < category.length(); i++) {
+      result += Math.pow(27, 250 - i - 1)*(1 + category.charAt(i) - 'a');
+    }
+    return result%databases.size();
   }
   
   public void addDatabase(String ipAddress, String key){
     if(!databases.contains(ipAddress)){
-      databases.add(ipAddress);
-      // some sort if timer or something
-      // repartition the keys based so 
+      databases.add(ipAddress); 
       repartion();
     }
   }
   
-
-  public boolean addCategory(){
+  public boolean addBook(String category, String contents){
+    int index = hash(category);
+    // call an RPC call with databases[index]
     return true;
-  }
-  
-  public boolean addBook(){
-    return true;
-  }
-
-  public static void initialDataLoad() {
-    // assign categories to databases
-    // divide caterogies with the number of dbs 
-
-    // randombly create ids and files for category, this is random access
-    
-    // send files to respective database for category --> ask barker 
-
-    // send the data to each database in other regions, using the add categories (potentially)
-    // and addbooks of other servers
   }
 
   private static void addCategories(){
@@ -97,6 +98,11 @@ public class FrontEndServer {
     return client;
   }
 
+  public boolean addFrontEnd(String ipAddress){
+    otherFronteEnds.add(ipAddress);
+    return true;
+  }
+
   /**
    * The main method
    */
@@ -106,8 +112,6 @@ public class FrontEndServer {
       return;
     }
     // pass in other frontend servers
-    otherFronteEnds.add("blah");
-    otherFronteEnds.add("blue");
 
     // add database IPs to be added to frontend
     for (String databaseIp: args) {
@@ -116,7 +120,6 @@ public class FrontEndServer {
     addCategories();
     
     
-
     try {
       PropertyHandlerMapping phm = new PropertyHandlerMapping();
       XmlRpcServer xmlRpcServer;
@@ -130,6 +133,4 @@ public class FrontEndServer {
       System.err.println("Server exception: " + e);
     }
   }
-
 }
-
