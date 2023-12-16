e0824fb16ed0c40195f8d19572431258903aa1a4
Eman Okyere <eokyere@bowdoin.edu>
2023-12-16 00:22:34 -0500

    kill servers script

---
+++ killServers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo "---------Starting Test 3---------" 
+
+#ireland
+#ssh 54.216.88.215 'killall java'
+#ssh 34.245.77.164 'killall java'
+
+
+ssh 13.114.101.52 'killall java'
+ssh 3.112.237.22 'killall java'
+
+
+#ssh "kill $(jobs -p)"
+
+#ssh "kill $(jobs -p)"
+#ssh "kill $(jobs -p)"
+
+
